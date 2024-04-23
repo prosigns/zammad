f907cd88b77ebe0e30d1447a6333f57e4756f3b2
Nayab Raheel <prosigns.dev@gmail.com>
2024-04-23 05:48:28 +0500

    remove package version from before.sh

--- contrib/packager.io/before.sh
+++ contrib/packager.io/before.sh
@@ -11,18 +11,7 @@ ruby -v
 env
 cat Gemfile.lock
 
-# Define default values if variables are not set
-: "${APP_PKG_ITERATION:=1}"  # Set iteration to 1 if not set
-
-# Use more detailed version information including packager.io build info.
-if [ -z "${APP_PKG_VERSION}" ]
-then
-  echo "Error: APP_PKG_VERSION not set, aborting."
-  exit 1
-fi
-
-ZAMMAD_VERSION="${APP_PKG_VERSION}-${APP_PKG_ITERATION}"
-echo "Setting VERSION information to $ZAMMAD_VERSION"
+echo "Setting VERSION information to 2.0.1"
 echo "$ZAMMAD_VERSION" > VERSION
 
 # cleanup
