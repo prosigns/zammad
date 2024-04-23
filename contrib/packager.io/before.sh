f0d027fda144fb12e84c671725cb2aff377259b5
Nayab Raheel <prosigns.dev@gmail.com>
2024-04-23 05:42:55 +0500

    2

--- contrib/packager.io/before.sh
+++ contrib/packager.io/before.sh
@@ -3,22 +3,25 @@
 # packager.io before script
 #
 
-set -eux
+set -euxo pipefail  # set the script to exit upon failure, undefined variables will cause script to exit
 
-# print environemnt
+# print environment
 uname -a
 ruby -v
 env
 cat Gemfile.lock
 
+# Define default values if variables are not set
+: "${APP_PKG_ITERATION:=1}"  # Set iteration to 1 if not set
+
 # Use more detailed version information including packager.io build info.
-if [ -z "${APP_PKG_ITERATION}" ]
+if [ -z "${APP_PKG_VERSION}" ]
 then
-  echo "Error: could not find version information, aborting."
+  echo "Error: APP_PKG_VERSION not set, aborting."
   exit 1
 fi
 
-ZAMMAD_VERSION="$APP_PKG_VERSION-$APP_PKG_ITERATION"
+ZAMMAD_VERSION="${APP_PKG_VERSION}-${APP_PKG_ITERATION}"
 echo "Setting VERSION information to $ZAMMAD_VERSION"
 echo "$ZAMMAD_VERSION" > VERSION
 
